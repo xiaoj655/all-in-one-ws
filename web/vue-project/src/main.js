07383bb2530305b474f544e01b576fe742ec6300
JKL <Brook@Brook-DI>
2023-11-13 13:08:24 +0800

    24

--- web/vue-project/src/main.js
+++
@@ -1,8 +0,0 @@
-import './assets/main.css'
-import store from './store/index'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-
-createApp(App).use(store).mount('#app')
--- web/web10/example.js
+++
@@ -1,4 +0,0 @@
-const str = '11kk'
-
-console.log(str.startsWith('21'))
-console.log(str.endsWith('kk'))
